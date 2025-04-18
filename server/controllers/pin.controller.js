81d51190d5bc7b8f3e918d1c2ddc3c8263d02a86
shyam-ounce <shyam.pareek@proounce.com>
2025-04-18 05:31:37 +0530

    added search query for fething pins.

--- server/controllers/pin.controller.js
+++ server/controllers/pin.controller.js
@@ -1,13 +1,23 @@
 import Pins from "../models/pin.model.js";
 
 const getPins = async (req, res) => {
-  const pageNumber = Number(req.query.page) || 0;
+  const pageNumber = Number(req.query.cursor) || 0;
+  const search = req.query.search;
 
-  const LIMIT = 21
+  const LIMIT = 21;
 
-  const pins = await Pins.find().limit(LIMIT).skip(pageNumber * LIMIT);
+  const pins = await Pins.find({
+    $or: [
+      { title: { $regex: search, $options: "i" } },
+      { tags: { $in: [search] } },
+    ],
+  })
+    .limit(LIMIT)
+    .skip(pageNumber * LIMIT);
 
   const hasNextPage = pins.length === LIMIT;
+
+  new Promise((resolve) => setTimeout(resolve, 2000));
 
   res.status(200).json({
     success: true,
